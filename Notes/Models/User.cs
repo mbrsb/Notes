149c0d7c84d0c1b3703e4206b803c204133d545d
mbrsb <laikening0407@gmail.com>
2015-07-09 12:48:45 +0800

    Finish UserModel

---
+++ Notes/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Notes.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage="请输入用户名！")]
+        public string username { get; set; }
+        [Required(ErrorMessage="请输入密码！")]
+        public string passwd { get; set; }
+        [Required(ErrorMessage="请输入邮箱！")]
+        public string email { get; set; }
+        public int confirm { get; set; }
+        public string way { get; set; }
+        public int Sync { get; set; }
+        public DateTime Add_Date { get; set; }
+        public DateTime? Upd_Date { get; set; }
+    }
+}
