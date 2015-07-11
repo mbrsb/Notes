c96041072b9ff8e2a8995f046fd5b2a4d54a9c2f
mbrsb <laikening0407@gmail.com>
2015-07-11 13:40:50 +0800

    UserController

---
+++ Notes/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Notes.Models;
+
+namespace Notes.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(User user)
+        {
+
+            return View();
+        }
+    }
+}
